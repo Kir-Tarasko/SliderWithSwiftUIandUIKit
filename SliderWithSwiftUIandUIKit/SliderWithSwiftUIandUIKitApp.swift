ca45c35e51e084e23c962b52ec49267c9f6aafab
Кирилл Тараско <tarasko.kirill@icloud.com>
2022-02-27 17:24:40 +0200

    Initial Commit

---
+++ SliderWithSwiftUIandUIKit/SliderWithSwiftUIandUIKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  SliderWithSwiftUIandUIKitApp.swift
+//  SliderWithSwiftUIandUIKit
+//
+//  Created by Кирилл Тараско on 27.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct SliderWithSwiftUIandUIKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

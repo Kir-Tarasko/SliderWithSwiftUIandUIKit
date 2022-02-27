add39a48d0f4cbda8948c6abacc19eb050fd87b8
Кирилл Тараско <tarasko.kirill@icloud.com>
2022-02-27 18:13:07 +0200

    Done

--- SliderWithSwiftUIandUIKit/ContentView.swift
+++ SliderWithSwiftUIandUIKit/ContentView.swift
@@ -8,9 +8,41 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var currentValue = 10.0
+    @State private var targetValue = Int.random(in: 0...100)
+    @State private var showAlert = false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("Подвиньте слайдер как можно ближе к: \(targetValue)")
+            HStack {
+                Text("0")
+                SliderUI(currentValue: $currentValue, color: .red, alpha: computeScore())
+            }
+            Text("100")
+        }
+        .padding()
+        Button("Проверь меня") {
+            showAlert.toggle()
+        }
+        .alert("Ваш счёт", isPresented: $showAlert, actions: {}) {
+            Text(String(computeScore()))
+        }
+        .padding()
+        Button("Начать заново") {
+            changeValue()
+        }
+    }
+    
+    private func changeValue() {
+        let newValue = Int.random(in: 0...100)
+        targetValue = newValue
+    }
+    
+    private func computeScore() -> Int {
+        let difference = abs(targetValue - lround(currentValue))
+        return 100 - difference
     }
 }
 
@@ -19,3 +51,4 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
